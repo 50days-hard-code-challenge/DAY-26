de5a31f8febe3a374dabfee7a38ce8b371d22b3e
Parth-88 <112095322+Parth-88@users.noreply.github.com>
2023-10-20 22:31:08 +0530

    Create Parth 1 (maxDepth).cpp

---
+++ Parth 1 (maxDepth).cpp
@@ -0,0 +1,11 @@
+lass Solution {
+public:
+   int maxDepth(TreeNode* root) {
+       
+        if(!root) return 0;
+
+        int maxLeft = maxDepth(root->left);
+        int maxRight = maxDepth(root->right);
+        return max(maxLeft, maxRight)+1;
+    }
+};
